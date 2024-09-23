0426ed2a581323fcd7ce8ca301ce6e4fb317e347
davis <davisochieng4@gmail.com>
2024-09-23 07:51:03 +0300

    Add model form

--- events/urls.py
+++ events/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('events/', views.all_events, name='all_events'),
     path('event/<int:id>/', views.event_detail, name='detail'),
+    path('event/<int:id>/add-venue/', views.add_venue, name='add_venue'),
+
 ]
--- events/views.py
+++ events/views.py
@@ -2,11 +2,12 @@ from datetime import date
 import calendar
 from calendar import HTMLCalendar
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
-from .models import Event
+from .models import Event, Venue
+from .forms import VenueForm
 
 
 def index(request, year=date.today().year, month=date.today().month):
@@ -28,3 +29,12 @@ def all_events(request):
 def event_detail(request, id):
     event = get_object_or_404(Event, id=id)
     return render(request, 'events/detail.html', {'event': event})
+
+
+def add_venue(request, id):
+    event = get_object_or_404(Event, id=id)
+    form = VenueForm(request.POST or None, instance=event.venue)
+    if form.is_valid():
+        form.save()
+        return redirect(event.get_absolute_url())
+    return render(request, 'events/add_venue.html', {'form': form})
