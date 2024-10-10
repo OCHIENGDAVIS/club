400feda130bbca2a81cf16f8ab32f6a20b8aa2d9
davis <davisochieng4@gmail.com>
2024-10-10 04:45:07 +0300

    Add admin customizations

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,33 @@
+import csv
+
 from django.contrib import admin
 from django.contrib.admin.decorators import register
+from django.http import HttpResponse
 
 from .models import Event, Venue
+
+admin.site.site_header = 'MyClub Administration'
+admin.site.site_title = 'MyClub Site Admin'
+admin.site.index_title = 'MyClub Site Admin Home'
+
+
+def export_to_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="event_export.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['name', 'event_date', 'venue', 'description'])
+    for record in queryset:
+        rec_list = []
+        rec_list.append(record.event_name)
+        rec_list.append(record.event_date.strftime('%m%d%Y, %H:%M'))
+        rec_list.append(record.description)
+        writer.writerow(rec_list)
+    return response
+
+
+export_to_csv.short_description = 'export to csv'
+
+export_to_csv.short_description = 'Export selected events to csv file'
 
 
 @register(Event)
@@ -21,8 +47,17 @@ class EventAdmin(admin.ModelAdmin):
                  })
 
     )
+    actions = [export_to_csv]
+
+
+class EventInline(admin.StackedInline):
+    model = Event
+    fields = ('event_name', 'event_date', 'description', 'venue', 'manager')
+    extra = 31
 
 
 @register(Venue)
 class VenueAdmin(admin.ModelAdmin):
     list_display = ['name', 'address']
+    list_display_links = ('name',)
+    # inlines = [EventInline]
